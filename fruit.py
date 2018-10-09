e03e60c3041cf9a297ef7123d7a0a1edde53e9de
jianghongsun <1456098533@qq.com>
2018-10-09 12:23:06 +0800

    Update fruit.py

--- fruit.py
+++ fruit.py
@@ -1,156 +1,5 @@
 #训练代码
-import os
-from tqdm import tqdm
-import tensorflow as tf
-import numpy as np
-import pandas as pd
-import skimage
-from skimage import io, transform
-from IPython.display import Image, display
-
-
-import keras
-from keras.models import Sequential,model_from_json
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, BatchNormalization
-from keras.layers import LSTM, Input
-from keras.models import Model
-from keras.optimizers import Adam
-import keras.backend.tensorflow_backend as KTF
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.3
-session = tf.Session(config=config)
-KTF.set_session(session)
-
-
-
-
-img_size = 100
-train_dir = './data/fruits/train/'
-test_dir = './data/fruits/test/'
-
-def get_data(folder_path):
-    imgs = []
-    indices = []
-    labels = []
-    for idx, folder_name in enumerate(os.listdir(folder_path)[:34]):
-        if not folder_name.startswith('.'):
-            labels.append(folder_name)
-            for file_name in tqdm(os.listdir(folder_path + folder_name)):
-                if not file_name.startswith('.'):
-                    img_file = io.imread(folder_path + folder_name + '/' + file_name)
-                    if img_file is not None:
-                        img_file = transform.resize(img_file, (img_size, img_size))
-                        imgs.append(np.asarray(img_file))
-                        indices.append(idx)
-    imgs = np.asarray(imgs)
-    indices = np.asarray(indices)
-    labels = np.asarray(labels)
-    return imgs, indices, labels
-
-X_train, y_train, train_labels = get_data(train_dir)
-
-X_test, y_test, test_labels = get_data(test_dir)
-
-num_categories = len(np.unique(y_train))
-
-new_X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2], X_train.shape[3]).astype('float32')
-new_X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2], X_test.shape[3]).astype('float32')
-new_y_train = keras.utils.to_categorical(y_train, num_categories)
-new_y_test = keras.utils.to_categorical(y_test, num_categories)
-
-def evaluate_model(model, batch_size, epochs):
-    history = model.fit(new_X_train, new_y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(new_X_test, new_y_test))
-    score = model.evaluate(new_X_test, new_y_test, verbose=0)
-    print('***Metrics Names***', model.metrics_names)
-    print('***Metrics Values***', score)
-convolutional = Sequential()
-
-convolutional.add(Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=(X_train.shape[1], X_train.shape[2], X_train.shape[3],)))
-convolutional.add(Conv2D(64, (3, 3), activation='relu'))
-convolutional.add(MaxPooling2D(pool_size=(2, 2)))
-convolutional.add(Dropout(0.35))
-
-convolutional.add(Conv2D(128, kernel_size=(3,3), activation='relu'))
-convolutional.add(Conv2D(256, (3, 3), activation='relu'))
-convolutional.add(MaxPooling2D(pool_size=(2, 2)))
-convolutional.add(Dropout(0.35))
-
-convolutional.add(Flatten())
-convolutional.add(Dense(512, activation='relu'))
-convolutional.add(Dropout(0.6))
-convolutional.add(BatchNormalization())
-convolutional.add(Dense(num_categories, activation='softmax'))
-
-convolutional.summary()
-convolutional.compile(loss="categorical_crossentropy", optimizer=Adam(lr=.0005), metrics=['accuracy'])
-
-# Save model to disk
-evaluate_model(convolutional, 32, 5)
-model_json = convolutional.to_json()
-with open("cnn_model.json", "w") as json_file:
-    json_file.write(model_json)
-convolutional.save_weights("cnn_model.h5")
-print("CNN model saved to disk")
-
-# Load model from disk
-json_file = open('cnn_model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-loaded_model.load_weights("cnn_model.h5")
-print("CNN model loaded from disk")
-
-# y_pred = loaded_model.predict(new_X_test, batch_size=None, verbose=0, steps=None).argmax(axis=-1)
-y_pred = loaded_model.predict(new_X_test, None, 0).argmax(axis=-1)
-
-res_crosstab = pd.crosstab(y_pred, y_test)
-
-dict_idx_fruit = {idx: label for idx, label in enumerate(test_labels)}
-print(dict_idx_fruit)
-
-res_crosstab
-
-for idx in range(num_categories):
-    accuracy = res_crosstab.loc[idx, idx] / res_crosstab.loc[:, idx].sum()
-    flag = '***LOW***' if accuracy < 0.75 else ''
-    print(dict_idx_fruit[idx])
-    print('   ', flag, 'accuracy –', round(accuracy * 100, 2), '%')
-
-def get_one_img_per_fruit(folder_path):
-    printouts = []
-    for idx, folder_name in enumerate(os.listdir(folder_path)[:34]):
-        if not folder_name.startswith('.'):
-            for idx2, file_name in enumerate(tqdm(os.listdir(folder_path + folder_name))):
-                if idx2 == 50:
-                    if not file_name.startswith('.'):
-                        img_filename = folder_path + folder_name + '/' + file_name
-                        display(Image(filename=img_filename))
-
-                        current_img = io.imread(img_filename)
-                        current_img = transform.resize(current_img, (img_size, img_size))
-                        current_img = np.asarray(current_img)
-                        current_img = np.asarray([current_img])
-
-                        # current_pred = loaded_model.predict(current_img, batch_size=None, verbose=0, steps=None).argmax(
-                        #     axis=-1)
-                        current_pred = loaded_model.predict(current_img, batch_size=None, verbose=0).argmax(
-                            axis=-1)
-                        current_pred = dict_idx_fruit[current_pred[0]]
-
-                        is_incorrect = 'INCORRECT' if folder_name != current_pred else ''
-
-                        msg = '{} – predicted as {} {}'.format(folder_name, current_pred, is_incorrect)
-                        print(msg)
-                        printouts.append(msg)
-    return printouts
-
-
-printouts = get_one_img_per_fruit(test_dir)
-
-for msg in printouts:
-    print(msg)
+# 训练模型
 
 import os
 from tqdm import tqdm
@@ -167,6 +16,10 @@ from keras.layers import LSTM, Input
 from keras.models import Model
 from keras.optimizers import Adam
 import keras.backend.tensorflow_backend as KTF
+import os
+import matplotlib.pyplot as plt # plt 用于显示图片
+import matplotlib.image as mpimg # mpimg 用于读取图片
+import numpy as np
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 config = tf.ConfigProto()
@@ -174,18 +27,14 @@ config.gpu_options.per_process_gpu_memory_fraction = 0.9
 session = tf.Session(config=config)
 KTF.set_session(session)
 
-
-
-
 img_size = 100
 train_dir = './data/fruits/train/'
 test_dir = './data/fruits/test/'
-
 def get_data(folder_path):
     imgs = []
     indices = []
     labels = []
-    for idx, folder_name in enumerate(os.listdir(folder_path)[:34]):
+    for idx, folder_name in enumerate(os.listdir(folder_path)[:35]):
         if not folder_name.startswith('.'):
             labels.append(folder_name)
             for file_name in tqdm(os.listdir(folder_path + folder_name)):
@@ -201,10 +50,7 @@ def get_data(folder_path):
     return imgs, indices, labels
 
 X_train, y_train, train_labels = get_data(train_dir)
-
 X_test, y_test, test_labels = get_data(test_dir)
-
-
 
 num_categories = len(np.unique(y_train))
 
@@ -273,7 +119,7 @@ for idx in range(num_categories):
 
 def get_one_img_per_fruit(folder_path):
     printouts = []
-    for idx, folder_name in enumerate(os.listdir(folder_path)[:34]):
+    for idx, folder_name in enumerate(os.listdir(folder_path)[:35]):
         if not folder_name.startswith('.'):
             for idx2, file_name in enumerate(tqdm(os.listdir(folder_path + folder_name))):
                 if idx2 == 50:
@@ -304,6 +150,8 @@ printouts = get_one_img_per_fruit(test_dir)
 
 for msg in printouts:
     print(msg)
+
+
 
 	
 	
